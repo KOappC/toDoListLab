5f05ee84a46796b8f57f8c010777025aeb4529c4
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-14 16:31:35 -0500

    initial commit

---
+++ controller.js
@@ -0,0 +1,19 @@
+(function() {
+    function FormController() {
+        var vm = this;
+        vm.toDoList = ["Grocery Shop", "Laundry", "Get Gas", "Vacuum"];
+        vm.addToDo = function(index) {
+            vm.toDoList.push(index);
+        };
+        vm.removeToDo = function(index) {
+            vm.toDoList.splice(index, 1)
+        };
+    }
+
+
+
+
+    angular
+        .module("moduleApp")
+        .controller("FormController", FormController)
+})();
