cfb0e7b4362bff39a49d3ffa8938157c771a06a5
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-16 16:29:54 -0500

    completed part two of the lab

--- scripts/task-form.js
+++ scripts/task-form.js
@@ -3,18 +3,18 @@
         controller: "FormController",
         template: `
         <div class="totalList">
-        <div class="listTitle">
-            <p id="toDoTitle">TODO List</p>
-            <p id="toDoSubtitle">A place to store the things you have to do!</p>
-        </div>
+            <div class="listTitle">
+                <p id="toDoTitle">TODO List</p>
+                <p id="toDoSubtitle">A place to store the things you have to do!</p>
+            </div>
 
-        <input type="text" ng-model="searchToDo" placeholder="Filter your to-dos">
-        
-        <task-list></task-list>
-        
-        <input type="text" ng-model="$ctrl.newToDo" placeholder="Add you to-do">
-        <button type="button" ng-click="$ctrl.addToDo($ctrl.newToDo)" hover-pink-directive="">Add</button>
-    </div>
+            <input type="text" ng-model="$ctrl.searchToDo" placeholder="Filter your to-dos">
+            <ol>
+                <task-list to-do-list="$ctrl.toDoList" search-to-do="$ctrl.searchToDo"></task-list>
+            </ol>
+            <input type="text" ng-model="$ctrl.newToDo" placeholder="Add you to-do">
+            <button type="button" ng-click="$ctrl.addToDo($ctrl.newToDo)" hover-pink-directive="">Add</button>
+        </div>
         `
     };
 
--- scripts/task-list.js
+++ scripts/task-list.js
@@ -1,10 +1,12 @@
 (function() {
     var taskList = {
+        bindings: {
+            toDoList: "<",
+            searchToDo: "<"
+        },
         controller: "FormController",
         template: `
-        <ol>
-            <li ng-repeat="things in $ctrl.toDoList | filter: searchToDo track by $index"> {{ things }} <i ng-click="$ctrl.removeToDo($index)" class="material-icons md-18" hover-directive>clear</i></li>
-        </ol>
+            <li ng-repeat="things in $ctrl.toDoList | filter: $ctrl.searchToDo track by $index"> {{ things }} <i ng-click="$ctrl.removeToDo($index)" class="material-icons md-18" hover-directive>clear</i></li>
         `
     };
 
